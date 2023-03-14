caa2ee26a1bb67e72e7e53c921492c2ccaca6bd1
Joachim Rorbol <jr@efio.dk>
2023-03-14 11:46:44 +0100

    Fixed issue with output errors for domains w. dnssec disabled

--- output.tf
+++ output.tf
@@ -1,11 +1,11 @@
 output "zones" {
   value = { for domain, zone in aws_route53_zone.this : domain => {
     name_servers = zone.name_servers
-    dnssec = {
+    dnssec = try({
       key_tag                = aws_route53_key_signing_key.dnssec[domain].key_tag
       digest                 = aws_route53_key_signing_key.dnssec[domain].digest_value
       digest_type            = aws_route53_key_signing_key.dnssec[domain].digest_algorithm_type
       signing_algorithm_type = aws_route53_key_signing_key.dnssec[domain].signing_algorithm_type
-    }
+    }, {})
   } }
 }
