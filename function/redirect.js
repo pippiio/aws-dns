b65a6700a04799aae72b627e0ba0b3f8e3cc731d
Joachim Rorbol <jr@efio.dk>
2023-09-23 12:41:50 +0200

    Added <uri> and <query> placeholder support in redirect uri

--- function/redirect.js
+++ function/redirect.js
@@ -2,11 +2,22 @@ function handler(event) {
   var statusCode = 301;
   var description = 'Moved Permanently';
   var headers = {}
+  var uri = (typeof event.request.uri !== 'undefined' && event.request.uri !== null) ? event.request.uri : "";
+  var query = "";
+
+  if (event.request.querystring) {
+    var queryString = event.request.querystring;
+    var queryStringArray = [];
+    for (var key in queryString) {
+        queryStringArray.push(`${key}=${queryString[key].value}`);
+    }
+    query = '?' + queryStringArray.join('&');
+  }
 
   switch (event.request.headers.host.value) {
 %{ for domain, location in redirects ~}
     case '${domain}':
-      var headers = {'location': { value: '${location}' }}
+      headers = {'location': { value: '${location}'.replace('<uri>', uri).replace('<query>', query) }}
       break;
 %{ endfor ~}
 
