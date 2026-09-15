e065f99917facdab4aa7fe7eec9dc3b4dfb43eca
Aluna0001 <cocobuntus@gmail.com>
2026-09-15 16:07:57 +0200

    Added DKIM test setup

---
+++ test/customer/main.tf
@@ -0,0 +1,9 @@
+module "dns" {
+  source = "./.."
+  // Variables can be given as imnput here to the module
+  domains = { for _, domain in local.domains : _ => merge(domain, { records = try(merge(domain.records...), {}) }) }
+}
+
+locals {
+  domains = yamldecode(file("./test.yml"))
+}
---
+++ test/dkim/provider.tf
@@ -0,0 +1,6 @@
+provider "aws" {
+  region                      = "eu-west-1"
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  skip_metadata_api_check     = true
+}
